a8270e26a8d39f5aaaf076cf9798294b9593200d
nuri <hendennurullah@gmail.com>
2022-06-27 15:38:32 +0200

    cleaned driver.c

--- driver.c
+++ driver.c
@@ -1,32 +1,27 @@
 #include "stdlib.h"
-// status
-#define ready 00
-#define busy 01
-#define error 02
 
-// command
-#define idle 00
-#define read 01
-#define write 02
-#define reset 04
-#define delete 08
+enum status{
+    READY = 00,
+    BUSY = 01,
+    ERROR = 02,
+}statusConst;
+enum command{
+    IDLE = 00,
+    READ = 01,
+    WRITE = 02,
+    RESET = 04,
+    DELETE = 8,
+}commandConst;
+#define KIB 1024
 
-#define Kib 1024
+unsigned char status = READY;
+unsigned char command = IDLE;
 
-unsigned char status;
-unsigned char command;
-//unsigned char data[20];
+unsigned char data[20];
 unsigned  char internerspeicher[20];
-unsigned char *data;
-void init() {
-    data = malloc(20);
-    status = ready;
-    command = idle;
-}
 
 void loop(){
-    if(command==write){
-        //
+
     }
-}
+
 
--- main.c
+++ main.c
@@ -2,6 +2,6 @@
 #include "stdio.h"
 
 int main(int argc, char const *argv[]) {
-    init();
+
     return 0;
 }
