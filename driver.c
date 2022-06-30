88e1120802eff3a5b758355ef26e459498e26f3a
nuri <hendennurullah@gmail.com>
2022-06-30 12:31:30 +0200

    teilaufgabe 3 progress

--- driver.c
+++ driver.c
@@ -1,57 +1,118 @@
-#include "string.h"
-#include "stdio.h"
+#include <string.h>
+#include <stdio.h>
+#include  <pthread.h>
 
-enum status{
+enum status {
     READY = 0,
     BUSY = 1,
     ERROR = 2,
 };
-enum command{
+enum command {
     IDLE = 0,
     READ = 1,
     WRITE = 2,
     RESET = 4,
     DELETE = 8,
 };
-#define KIB 1024
 
+#define KIB 1024
 unsigned char status = READY;
 unsigned char command = IDLE;
 
 unsigned char data[20];
-unsigned  char internerspeicher[KIB];
+static unsigned char internerspeicher[KIB];
 
-static int  indexInternetSpeicher = 0;
+static int indexInternerSpeicher = 0;
 
-void cmdWrite(){
-    //Copy data to internalMemory and delete data
-    //strcat(&internerspeicher,&data);
-    memcpy(&internerspeicher + indexInternetSpeicher ,&data, strlen(data));
-    indexInternetSpeicher += strlen(data) + 1 ;
-    memset(&data[0],0, sizeof(data));
+pthread_t IOManager;
+
+void cmdWrite() {
+    status = BUSY;
+    command = IDLE;
+    //Copy data to internalMemory
+    memcpy(internerspeicher + indexInternerSpeicher, data, sizeof(data));
+
+    // task wants us to use sizeof() below, but that would make printing
+    // difficult, because C stops the string at the first 0 encounter
+    indexInternerSpeicher += strlen(data);
+
+    //delete data
+    memset(&data[0], 0, sizeof(data));
+
+    // no space left in "internerspeicher"
+    if (indexInternerSpeicher > 1003) {
+        status = ERROR;
+    }
+    else  status = READY;
 }
-void cmdRead(){
-    printf("%s\n", internerspeicher);
+
+void cmdRead() {
+    command = IDLE;
+    for (int i = 0; i < sizeof(internerspeicher)/ sizeof(data); i++) {
+        memcpy(data, internerspeicher, sizeof(data));
+        printf("%s\n", data);
+    }
+    status = READY;
+}
+
+void cmdDelete() {
+    command = IDLE;
+    memset(&data[0], 0, sizeof(data));
+    memset(&internerspeicher[0], 0, sizeof(internerspeicher));
+    indexInternerSpeicher = 0;
+    status = READY;
+}
+
+void cmdReset() {
+    command = IDLE;
+    indexInternerSpeicher = 0;
+    status = READY;
+}
+
+void *dataProcessor(void *arg) {
+    // not sure what to do when status is BUSY and command is idle
+    while (1) {
+        if (status != BUSY) {
+            if (status == ERROR) {
+                fprintf(stderr, "");
+            }
+            if (command == WRITE && strlen(data) > 0) {
+                cmdWrite();
+            }
+            if (command == DELETE) {
+                cmdDelete();
+            }
+            if (command == RESET) {
+                cmdReset();
+            }
+            if (command == READ) {
+                cmdRead();
+            }
+        }
+    }
+    return NULL;
+}
+
+void init(){
+    pthread_create(&IOManager, NULL, dataProcessor, NULL);
+}
+
+
+void test() {
+    data[0] = 'm';
+    data[1] = 'e';
+    data[2] = 'i';
+    data[3] = 'n';
+    data[4] = 'a';
+    cmdWrite();
+    cmdRead();
+    data[0] = 't';
+    data[1] = 'e';
+    data[2] = 's';
+    data[3] = 't';
+    cmdWrite();
+    cmdRead();
 
 }
-void cmdDelete(){
-    memset(&data[0],0, sizeof(data));
-    memset(&internerspeicher[0],0, sizeof(internerspeicher));
-}
 
-void test(){
-        data[0] = 'm';
-        data[1] = 'e';
-        data[2] = 'i';
-        data[3] = 'n';
-        data[4] = 'a';
-        cmdWrite();
-        cmdRead();
-       data[0] = 't';
-       data[1] = 'e';
-       data[2] = 's';
-       data[3] = 't';
-        cmdWrite();
-        cmdRead();
-}
 
--- main.c
+++ main.c
@@ -1,7 +1,56 @@
 #include "driver.c"
-#include "stdio.h"
+#include <unistd.h>
 
+//  inputData -> data -> internerspeicher
+void dataWrite(unsigned char inputData[]){
+    int iterationcount = sizeof(inputData)/20;
+    if(sizeof(inputData)%20 != 0 ) iterationcount++;
+
+    for (int i = 0; i < iterationcount; i++) {
+        for (int j = 0; j < 20 ; j++) {
+            data[i] = 'x';
+        }
+        command = WRITE;
+        status = BUSY;
+        // @ TODO status == ERROR case
+        if(status != READY) sleep(2);
+    }
+}
+//  loop(internerspeicher -> data and output)
+void dataRead(){
+    command = READ;
+    status = BUSY;
+    if(status != READY) sleep(2);
+}
+// sets read/write pointer from internerspeicher to 0
+void dataReset(){
+    command = RESET;
+    status = BUSY;
+    if(status != READY) sleep(2);
+}
+// Deletes everything inside data and internerspeicher and sets pointer to 0
+void dataDelete(){
+    command = DELETE;
+    status = BUSY;
+    if(status != READY) sleep(2);
+}
 int main(int argc, char const *argv[]) {
-    test();
+    init();
+
+    unsigned char data512[512];
+    unsigned char data2048[2048];
+    unsigned char data252[252];
+
+    dataWrite(data512);
+    dataRead();
+    dataReset();
+    dataDelete();
+
+    //dataWrite(data2048);
+    //dataWrite(252);
+
+
+
     return 0;
+
 }
